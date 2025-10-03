ae3af33bf6b02810413a9f659999efe01607664c
BookYolo Tech <tech@bookyolo.com>
2025-10-03 08:30:19 -0400

    Updated frontend

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -1239,8 +1239,8 @@ const ChatInterface = () => {
         
         {/* Sidebar */}
         <div className={`
-          fixed lg:static inset-y-0 left-0 z-[80] lg:z-auto
-          w-80 border-r border-accent bg-white shadow-xl lg:shadow-none p-3 h-full flex flex-col
+          fixed inset-y-0 left-0 z-[80]
+          w-80 border-r border-accent bg-white shadow-xl p-3 h-full flex flex-col
           transform transition-transform duration-300 ease-in-out
           ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
         `}>
